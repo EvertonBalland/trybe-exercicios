4f5210f1a589bb7fcb32654ddd41d5e92ce48527
EvertonBalland <ballandeverton@gmail.com>
2023-05-12 21:43:51 +0100

    resolve exercicio DOM e Seletores

---
+++ fundamentos/secao-04-javascript-dom-eventos-webstorsge/dia-01-javascript-dom-e-seletores/Exercicio1/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+      paragraph.style.color = "red";
+
+const title = document.getElementById('page-title');
+title.innerText = 'November Rain';
+title.style.color = 'green';
+title.style.fontSize = '60px';
+const secondParagraph = document.getElementById('second-paragraph');
+      secondParagraph.style.backgroundColor = 'grey';
+const subtitle = document.getElementById('subtitle');
+subtitle.style.fontStyle = 'italic';
+subtitle.style.fontSize = '30px';
