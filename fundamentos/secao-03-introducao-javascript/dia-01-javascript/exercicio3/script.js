66f75bea1cb330ac65298ee099e9ab87295f6091
EvertonBalland <ballandeverton@gmail.com>
2023-05-16 00:06:42 +0100

    exercicio 3

---
+++ fundamentos/secao-03-introducao-javascript/dia-01-javascript/exercicio3/script.js
@@ -0,0 +1,11 @@
+let a = 6;
+let b = 10;
+let c = 12;
+
+if (a > b && a > c) {
+    console.log('O maior número é:'+ a + '(a)');
+} else if(b > a && b > c) {
+    console.log('Omaior número é:' + b + '(b)');
+} else {
+    console.log('O maior número é:' + c + '(c)');
+}
